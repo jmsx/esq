0d1d718cd35d0573b3dd9e2fd2118a2eb0ae715f
jmsx <contacto@jmsx.es>
2022-02-09 13:00:14 +0100

    Update Whitenoise

--- backend/esq/settings.py
+++ backend/esq/settings.py
@@ -16,7 +16,7 @@ import os
 import django_heroku
 
 
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -172,3 +172,5 @@ SIMPLE_JWT = {
 
 # Collect static files
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
